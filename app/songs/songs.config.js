2a88a891f3e086c410dd0f9fbd02853db1145b9e
oolawo09 <aolawo@alueducation.com>
2017-06-15 07:42:26 +0400

    songs and mixtapes working with youtube embeds

--- app/songs/songs.config.js
+++ app/songs/songs.config.js
@@ -1,15 +1,24 @@
-"use strict"; 
+"use strict";
 
 angular.module("songs")
-	.config(function($stateProvider) {
+    .config(function($stateProvider, $sceDelegateProvider) {
+
+        //allow urls from other sites 
+        $sceDelegateProvider.resourceUrlWhitelist([
+            // Allow same origin resource loads.
+            'self',
+            // Allow loading from our assets domain.  Notice the difference between * and **.
+            '*://www.youtube.com/**'
+        ])
+
 
         $stateProvider
 
         // Article detail pages ========================================
-        .state("songs", {
-          url: "/songs/:songId", 
-          templateUrl: "songs/songs.html", 
-          controllerAs: "vm", 
-          controller: "SongsController"
-        }); 
-     }); 
+            .state("songs", {
+            url: "/songs/:songId",
+            templateUrl: "songs/songs.html",
+            controllerAs: "vm",
+            controller: "SongsController"
+        });
+    });
