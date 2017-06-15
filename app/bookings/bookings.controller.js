f1adf7d889fedde6b8535e493bde4db2b2a87c5c
oolawo09 <aolawo@alueducation.com>
2017-06-15 07:47:18 +0400

    bookings page

--- app/bookings/bookings.controller.js
+++ app/bookings/bookings.controller.js
@@ -1,13 +1,14 @@
 "use strict";
 
 angular.module("about")
-    .controller("Bookings", function() {
+    .controller("Bookings", function($state) {
         var vm = this;
 
         vm.bookingDetails = {};
 
 
         vm.book = function(bookingDetails) {
-
+        	console.log("here")
+        	$state.go("home")
         }
     })
