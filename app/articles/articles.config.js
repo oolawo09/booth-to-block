8af56451ee3cc26d413bfff0bbc33ac63b8e68b0
oolawo09 <aolawo@alueducation.com>
2017-07-11 13:49:45 +0400

    actual articles text added

--- app/articles/articles.config.js
+++ app/articles/articles.config.js
@@ -1,15 +1,23 @@
-"use strict"; 
+"use strict";
 
 angular.module("articles")
-	.config(function($stateProvider) {
+    .config(function($stateProvider, $sceDelegateProvider) {
+
+        //allow urls from other sites 
+        $sceDelegateProvider.resourceUrlWhitelist([
+            // Allow same origin resource loads.
+            'self',
+            // Allow loading from our assets domain.  Notice the difference between * and **.
+            '*://www.youtube.com/**'
+        ])
 
         $stateProvider
 
         // Article detail pages ========================================
-        .state("articles", {
-          url: "/articles/:articleId", 
-          templateUrl: "articles/articles.html", 
-          controllerAs: "vm", 
-          controller: "ArticlesController"
-        }); 
-     }); 
+            .state("articles", {
+            url: "/articles/:articleId",
+            templateUrl: "articles/articles.html",
+            controllerAs: "vm",
+            controller: "ArticlesController"
+        });
+    });
